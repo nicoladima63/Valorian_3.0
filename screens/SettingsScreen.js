65f18b75b9d8a31519b19cdb87f256bba55369aa
nicoladima63 <nicoladimartino@gmail.com>
2024-06-28 17:40:56 +0200

    aggiunto effetto a lcambio tema

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppContext } from '../context/AppContext';
 import { useTheme } from '../context/ThemeContext';
 import Layout from './Layout';
-import { View, Text, Animated, Dimensions } from 'react-native';
+import { View, Text, Animated, Dimensions, Pressable, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { RadioButton } from 'react-native-paper';
 
@@ -38,50 +38,50 @@ const SettingsScreen = ({ navigation }) => {
             header={
                 <Text style={[theme.h4, theme.mb20, theme.mt10, theme.ml20]}>Impostazioni</Text>
             }
-            //footer={<Text>Another Footer</Text>}
-            //fab={<Text style={theme.fabText}>+</Text>}
+        //footer={<Text>Another Footer</Text>}
+        //fab={<Text style={theme.fabText}>+</Text>}
         >
             <View style={[theme.body, { borderTopColor: theme.colors.slate7, borderTopWidth: 1, paddingTop: 10 }]}>
                 <Text style={[theme.text, theme.mb10, theme.ml10, { color: theme.colors.slate11 }]}>Tema</Text>
                 <RadioButton.Group onValueChange={handleToggleTheme} value={themeMode}>
-                    <View style={[theme.article, theme.articleTop]}>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                <Icon name="adjust" size={20} color={theme.colors.slate12} style={{ marginRight: 20 }} />
-                                <Text style={theme.text}>Automatico</Text>
+                    <TouchableOpacity onPress={() => handleToggleTheme('auto')}>
+                        <View style={[theme.article, theme.articleTop]}>
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                    <Icon name="adjust" size={20} color={theme.colors.slate12} style={{ marginRight: 20 }} />
+                                    <Text style={theme.text}>Automatico</Text>
+                                </View>
+                                <RadioButton value="auto" />
                             </View>
-                            <RadioButton value="auto" />
                         </View>
-
-                    </View>
-
-                    <View style={[theme.article, theme.articleMiddle]}>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                <Icon name="sun-o" size={20} color={theme.colors.slate12} style={{ marginRight: 20 }} />
-                                <Text style={theme.text}>Chiaro</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => handleToggleTheme('light')}>
+                        <View style={[theme.article, theme.articleMiddle]}>
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                    <Icon name="sun-o" size={20} color={theme.colors.slate12} style={{ marginRight: 20 }} />
+                                    <Text style={theme.text}>Chiaro</Text>
+                                </View>
+                                <RadioButton value="light" color={theme.colors.slate12} />
                             </View>
-                            <RadioButton value="light" color={theme.colors.slate12} />
                         </View>
-                    </View>
-                    <View style={[theme.article, theme.articleBottom]}>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                <Icon name="moon-o" size={20} color={theme.colors.slate12} style={{ marginRight: 20 }} />
-                                <Text style={theme.text}>Scuro</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => handleToggleTheme('dark')}>
+                        <View style={[theme.article, theme.articleBottom]}>
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                    <Icon name="moon-o" size={20} color={theme.colors.slate12} style={{ marginRight: 20 }} />
+                                    <Text style={theme.text}>Scuro</Text>
+                                </View>
+                                <RadioButton value="dark" />
                             </View>
-                            <RadioButton value="dark" />
                         </View>
-                    </View>
-
-
+                    </TouchableOpacity>
                 </RadioButton.Group>
                 <Text style={{ marginTop: 10, marginLeft: 15, color: theme.colors.slate10 }}>
                     <Text style={{ color: theme.colors.slate12 }}>Automatico </Text>
                     &egrave; supportato solo sui sistemi operativi che consentono di controllare i colori a livello di sistema.
                 </Text>
-
-
             </View>
         </Layout >
     )
