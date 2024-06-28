f62e0d67dd8bba8a7bfe7b9c48e902ee5e77a6bd
nicoladima63 <nicoladimartino@gmail.com>
2024-06-28 17:31:28 +0200

    aggieggiato al calendario ma ninete di buono

--- screens/Calendario.js
+++ screens/Calendario.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import * as Controller from '../controllers/bisogniController';
 import { useTheme } from '../context/ThemeContext';
-import { ScrollView, RefreshControl, View, Text, StyleSheet, Alert } from 'react-native';
+import { ScrollView, RefreshControl, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Calendar } from 'react-native-big-calendar';
 import 'dayjs/locale/it';
 import dayjs from 'dayjs';
@@ -59,9 +59,7 @@ export default function CalendarPage({ navigation }) {
         const monthName = date.toLocaleString('default', { month: 'long' });
         const year = date.getFullYear();
         return (
-            <View>
-                <Text style={styles.headerText}>{monthName} {year}</Text>
-            </View>
+            <Text style={theme.h2}>{monthName} {year}</Text>
         );
     };
 
@@ -75,62 +73,76 @@ export default function CalendarPage({ navigation }) {
     };
 
     const events = transformBisogniToEvents();
+
     const eventCellStyle = (event, start, end, isSelected) => {
         return {
             height: 20,
             alignItems: 'center',
             backgroundColor: event.color,
-            color: 'red',
+            color: theme.colors.slate12,
         };
     };
+    const cellStyle = () => {
+        return (
+            <View style={{ backgroundColor: theme.colors.slate12 }} />
+        );
+    };
+
+    const Header = () => (
+        <Text style={[theme.h4, theme.mb20, theme.mt10, theme.ml20, { backgroundColor: theme.colors.slate2 }]}>
+            Calendario delle soddisfazioni
+        </Text>
+    );
+
+    const BodyFooter = () => (
+        <View style={theme.bodyFooter}>
+            <TouchableOpacity onPress={handleCancel} style={theme.buttonCancel}>
+                <Text style={theme.buttonText}>Annulla</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleDelete} style={theme.buttonDelete}>
+                <Text style={theme.buttonText}>Elimina</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleOk} style={theme.buttonOK}>
+                <Text style={theme.buttonText}>OK</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleSave} style={theme.buttonSave}>
+                <Text style={theme.buttonText}>Salva</Text>
+            </TouchableOpacity>
+        </View>
+    );
+
+
 
     return (
         <Layout
             navigation={navigation}
             showTopBar={false}
-            header={<Text style={theme.headerTitle}></Text>}
+            header={<Header />}
+            //fab={<Fab />}
+            //fabAction={handlePressFab}
+            showBodyFooter={false}
+            bodyFooter={<BodyFooter />}
         >
-            <CustomText style={theme.headerTitle}>Calendario delle soddisfazioni</CustomText>
-            <CustomText style={theme.title}>{renderHeader(currentDate)}</CustomText>
+            <View style={ {backgroundColor: theme.colors.white}}>
+                <Text style={[theme.text, theme.h4, theme.fwb, theme.mb20]}>{renderHeader(currentDate)}</Text>
+                <View style={[theme.article, { height: 700, backgroundColor: theme.colors.white}]}>
 
-            <ScrollView
-                style={styles.container}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={refreshing}
-                        onRefresh={onRefresh}
+                    <Calendar
+                        toolbar={false}
+                        events={events}
+                        height={600}  // Altezza del calendario
+                        mode="month"  // Modalità di visualizzazione mensile
+                        locale="it"
+                        weekStartsOn={1}  // Imposta il primo giorno della settimana a lunedì
+                        cellStyle={cellStyle}
+                        eventCellStyle={eventCellStyle} // Applica lo stile personalizzato agli eventi
+                        currentDate={currentDate} // Usa lo stato aggiornato
+                        swipeEnabled={true} // Abilita lo swipe nativo del calendario
+                        onSwipeEnd={onSwipeEnd} // Gestisce l'evento di fine swipe
                     />
-                }
-            >
-                <Calendar
-                    events={events}
-                    height={600}  // Altezza del calendario
-                    mode="month"  // Modalità di visualizzazione mensile
-                    locale="it"
-                    weekStartsOn={1}  // Imposta il primo giorno della settimana a lunedì
-                    eventCellStyle={eventCellStyle} // Applica lo stile personalizzato agli eventi
-                    currentDate={currentDate} // Usa lo stato aggiornato
-                    swipeEnabled={true} // Abilita lo swipe nativo del calendario
-                    onSwipeEnd={onSwipeEnd} // Gestisce l'evento di fine swipe
-                />
-            </ScrollView>
+                </View>
+            </View>
         </Layout>
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 10,
-        backgroundColor: '#f0f0f0',
-    },
-    headerText: {
-        fontSize: 28,
-        color: 'black',
-    },
-});
